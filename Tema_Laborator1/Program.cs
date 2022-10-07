9bb5948a3fed31ab94911935de2e169913f70cbe
Ana <anaopris68@gmail.com>
2022-10-07 19:53:01 +0300

    Add project files.

---
+++ Tema_Laborator1/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Aplicatii
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Ex1
+            //Scrieti un program care citind de la tastatura cele trei dimensiuni ale unui paralelipiped dreptunghic, va afisa volumul lui.
+
+            //int lungime = 100;
+            //int latime = 50;
+            //int inaltime = 50;
+
+            //int calculamVolumulParalelipipedului = lungime * latime * inaltime;
+
+            //Console.WriteLine("Volumul paralelipipedului este : " + calculamVolumulParalelipipedului);
+
+
+
+            //Ex2
+            //Scrieti un program care va calcula media aritmetica a trei numere citite de la tastatura.
+
+            //int a = 10;
+            //int b = 30;
+            //int c = 60;
+
+            //int calculamMediaAritmetica = (a + b + c) / 3;
+
+            //Console.WriteLine("Media aritmetica este : " + calculamMediaAritmetica);
+
+
+
+            //Ex3
+            //Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastatura
+
+
+            //int a;
+
+            //Console.WriteLine("Introduceti un numar intreg");
+            //a = int.Parse(Console.ReadLine());
+
+            //int D = a % 10;
+            //while (D >=10)
+            //{
+            //    D /= 10;
+            //}
+            //Console.WriteLine("Ultima cifra = " + D);
+
+
+            //Ex4
+            //Scrieti un program care va afisa semnul unui numar de la tastatura
+
+            //Daca numarul este pozitiv va afisa +
+
+            //Daca numarul este negativ va afisa -
+
+            //Daca numarul este 0 va afisa 0
+
+
+            int a = int.Parse
+            a = Console.ReadLine(); 
+
+            if (a > 0)
+            {
+                Console.WriteLine("+");
+            }
+
+            else if (a < 0)
+            {
+                Console.Write("-");
+            }
+
+            else if (a == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else 
+                Console.WriteLine("Fals");
+        }
+    }
+}
+            
+        
+    
+            
